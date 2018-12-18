d0dc9a92b616ae065417fc84e14559788f7200ec
Renato Riccio <mail>
2018-12-19 00:18:29 +0100

    add index details output

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mongo-mix
+
+DISCLAIMER:
+!! Use these scripts at your own risk. Test them first !!
--- get_collections_info.js
+++ get_collections_info.js
@@ -1,3 +1,16 @@
+// This script prints for each collection:
+// - count
+// - size 
+// - storagesize
+// - avgObjSize
+// - totalIndexSize
+// - wiredTiger file name
+//
+// Note: each size is in byte, for a sharded cluster this information is printed per shard
+
+// Allow to hide detailed information on the indexes
+var index_info = true
+
 var shard_list = []
 db.getSiblingDB("config").getCollection("shards").find({},{_id:1}).forEach(function(d){
     shard_list.push(d._id)
@@ -11,15 +24,26 @@ db.getSiblingDB("config").getCollection("collections").find({key: {$exists: true
 db.adminCommand({listDatabases: 1}).databases.forEach(function(d) {
     var database = db.getSiblingDB(d.name);  
     database.getCollectionNames().forEach(function(c) { 
-        var stats = database.getCollection(c).stats();
+        var stats = database.getCollection(c).stats({indexDetails: index_info});
         print(d.name + "." + c)
         print("-----------------------------------")
         print("count: " + stats.count)
         print("size: " + stats.size)
+        print("storagesize: " + stats.storageSize)
+        print("avgObjSize: " + stats.avgObjSize)
         print("totalIndexSize: " + stats.totalIndexSize)
         namespace = d.name + "." + c
         if(sharded_collection.indexOf(namespace) == -1 ){
             print("file: " + stats.wiredTiger.uri.replace("statistics:table:", ""))
+            if(index_info == true){
+                for (var key in stats.indexDetails){
+                    index_stat = stats.indexDetails[key]
+                    print("+++++++++++++++++++")
+                    print("index: " + key)
+                    print("file: " + index_stat.uri.replace("statistics:table:", ""))
+                    print("size: " + stats.indexSizes[key])
+                }
+            }
         } else {
             shard_list.forEach(function(s){
                 nested_shard = stats.shards["shard01"]
@@ -27,11 +51,20 @@ db.adminCommand({listDatabases: 1}).databases.forEach(function(d) {
                 print("shard: " + s)
                 print("count: " + nested_shard.count)
                 print("size: " + nested_shard.size)
-                print("totalIndexSize: " + nested_shard.totalIndexSize)           
+                print("storagesize: " + nested_shard.storageSize)
+                print("avgObjSize: " + nested_shard.avgObjSize)
+                print("totalIndexSize: " + nested_shard.totalIndexSize) 
                 print("file: " + nested_shard.wiredTiger.uri.replace("statistics:table:", ""))
-
+                if(index_info == true){
+                    for (var key in nested_shard.indexDetails){
+                        index_stat = nested_shard.indexDetails[key]
+                        print("+++++++++++++++++++")
+                        print("index: " + key)
+                        print("file: " + index_stat.uri.replace("statistics:table:", ""))
+                        print("size: " + nested_shard.indexSizes[key])
+                    }
+                }
          })
         }
         print("################################################")
-    
     })})
